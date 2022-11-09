8c93abff9f9da60e2fa46460dc27412b47131def
FelipePy <felipespsampaiomotasp@gmail.com>
2022-11-09 17:46:57 -0300

    rename variable

--- src/pages/styled.js
+++ src/pages/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledHome = styled.div`
   background-color: ${(props) => props.theme.colors.background};
-  color: ${(props) => props.theme.colors.textColor};
+  color: ${(props) => props.theme.colors.text};
   border-color: ${(props) => props.theme.colors.borderBase};
 `;
 
